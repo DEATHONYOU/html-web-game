ea069adbc5d6a174521fc2ee396546d4bebcc6d3
DEATHONYOU <128713511+DEATHONYOU@users.noreply.github.com>
2023-12-11 11:35:06 -0600

    added some more valus to make it work

--- index.js
+++ index.js
@@ -20,18 +20,11 @@ const mpFrame = function() {
 }
 
 canvas.onclick = function() {
-  health -= 10;
-  healthBar.updateHealth(health);
+  mp -= 10;
+  healthBar.updatemp(mp);
 };
 
 mpFrame();
-const canvas = document.getElementById("canvas");
-const context = canvas.getContext("2d");
-const width = canvas.width = 320;
-const height = canvas.height = 480;
-
-canvas.style.marginTop = window.innerHeight / 2 - height / 2 + "px";
-
 let health = 100;
 const healthBarWidth = 200;
 const healthBarHeight = 30;
